8baac714890e0b6129269bdc79e6d6bd3194af43
Learn-Live <kun.bj@foxmail.com>
2018-10-14 11:32:31 -0400

    add try_except func

--- history_files/try_except_demo.py
+++ history_files/try_except_demo.py
@@ -1,6 +1,15 @@
 """
     #if num = 0.0, execute 'except',
     #if num = 0.1, execute 'else'
+
+
+    try:
+        statements # statements that can raise exceptions
+    except:
+        statements # statements that will be executed to handle exceptions
+    else:
+        statements # statements that will be executed if there is no exception
+
 """
 
 
