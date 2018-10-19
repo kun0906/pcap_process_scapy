ada2b3fda049b8ddf10e2a49bc64517ead4f1167
Learn-Live <kun.bj@foxmail.com>
2018-10-19 13:06:38 -0400

    # this function is deprected about using rdpcap() in scapy def pcap2sessions_statistic(input_file):

--- 3_pcap_parser/pcap2sessions_scapy.py
+++ 3_pcap_parser/pcap2sessions_scapy.py
@@ -339,7 +339,7 @@ def pcap2sessions_statistic_with_pcapreader_scapy(input_f):
 
     # Step 2. achieve all the session in pcap.
     # data.stats
-    pkts_stats = {'non_Ether_pkts': 0, 'non_IPv4_pkts': 0, 'non_TCP_UDP_pkts': 0, 'TCP_pkts': 0,
+    pkts_stats = {'non_Ether_IPv4_pkts': 0, 'non_IPv4_pkts': 0, 'non_TCP_UDP_pkts': 0, 'TCP_pkts': 0,
                   'UDP_pkts': 0}
     cnt = 0
     sess_dict = {}
@@ -369,8 +369,31 @@ def pcap2sessions_statistic_with_pcapreader_scapy(input_f):
                     # pkts_stats['IPv4_pkts'] += 1
             else:
                 pkts_stats['non_IPv4_pkts'] += 1
+        elif pkt.name =='Raw packet':
+            print('pkt.name : ', pkt.name)
+            if pkt.name.upper() in ['IP', 'IPV4']:
+                if pkt.payload.name.upper() in ["TCP", "UDP"]:
+                    if cnt == 0:
+                        print('packet[0] info: "%s:%d-%s:%d-%s"+%s' % (
+                            pkt.src, pkt.payload.sport, pkt.dst, pkt.payload.dport,
+                            pkt.payload.name, pkt.payload.payload))
+                    five_tuple = pkt.src + ':' + str(
+                        pkt.payload.sport) + '-' + pkt.dst + ':' + str(
+                        pkt.payload.dport) + '-' + pkt.payload.name.upper()
+                    save_session_to_dict(k=five_tuple, v=pkt, sess_dict=sess_dict)
+                    cnt += 1
+                    # pkts_lst.append(pkt.payload)  # only include "IPv4+IPv4_payload"
+                    if pkt.payload.name.upper() == "TCP":
+                        pkts_stats['TCP_pkts'] += 1
+                    else:
+                        pkts_stats['UDP_pkts'] += 1
+                else:
+                    pkts_stats['non_TCP_UDP_pkts'] += 1
+                    # pkts_stats['IPv4_pkts'] += 1
+            else:
+                pkts_stats['non_IPv4_pkts'] += 1
         else:
-            pkts_stats['non_Ether_pkts'] += 1
+            pkts_stats['non_Ether_IPv4_pkts'] += 1
 
     # data.stats
     # print('%s info is %s' % (input_f, pkts_lst))
@@ -396,151 +419,11 @@ def pcap2sessions_statistic_with_pcapreader_scapy(input_f):
                 S = str(pkt.payload.payload.fields['flags'])
                 # step 1. discern the begin of TCP session.
                 if 'S' in S:
-                    TCP_start_flg = True
                     if 'A' not in S:  # the first SYN packet in TCP session.
                         # if flags[S] == "SYN":
+                        TCP_start_flg = True
                         tcp_sess_list.append(pkt)
-                    else:  # the second SYN + ACK
-                        tcp_sess_list.append(pkt)
-                    continue
-                # step 2. discern the transmitted data of TCP session
-                if TCP_start_flg:  # TCP data transform.
-                    for pkt_t in v[i:]:
-                        tcp_sess_list.append(pkt_t)
-                        F = str(pkt_t.payload.payload.fields['flags'])
-                        if 'F' in F:  # if  flags[F]== "FIN":
-                            full_session_flg = True
-                        # step 3. discern the finish of TCP session.
-                        if 'S' in str(pkt_t.payload.payload.fields['flags']) and len(
-                                tcp_sess_list) >= 5:  # the second session
-                            print('the second session begins.')
-                            break
-                else:  # TCP_start_flg = False
-                    # print('TCP still does not begin...')
-                    pass
-                if full_session_flg:
-                    full_sess_dict[k] = tcp_sess_list
-                    # print('tcp_sess_list:', k, len(tcp_sess_list))
-                    break
-        elif prtl == "UDP":
-            # if len(v) < 2:
-            #     print('%s is not a UDP session.'%k)
-            # else:
-            #     full_sess_dict[k] = v
-            full_sess_dict[k] = v  # do not do any process for UDP session.
-        else:
-            pass
-    print('pkts_stats is ', pkts_stats)
-    print('Number of sessions(TCP/UDP) in %s is %d, number of full session(TCP/UDP) is %d' % (
-        input_f, len(sess_dict.keys()), len(full_sess_dict.keys())))
-    print('all_sess_dict:', count_protocls(sess_dict), '\nfull_sess_dict:', count_protocls(full_sess_dict))
-
-    all_stats_dict = {}
-    all_stats_dict['pkts_stats'] = pkts_stats
-    all_stats_dict['all_sess'] = count_protocls(sess_dict)
-    all_stats_dict['full_sess'] = count_protocls(full_sess_dict)
-    all_stats_dict['full_sess_size_distribution'] = count_sess_size(full_sess_dict)
-
-    print(all_stats_dict)
-
-    return all_stats_dict
-
-
-# this function is deprected about using rdpcap() in scapy
-def pcap2sessions_statistic(input_f):
-    """
-        achieve the statistic of full sessions in pcap after removing uncompleted TCP sessions
-        There is no process on UDP sessions
-
-        flags in scapy
-         flags = {
-        'F': 'FIN',
-        'S': 'SYN',
-        'R': 'RST',
-        'P': 'PSH',
-        'A': 'ACK',
-        'U': 'URG',
-        'E': 'ECE',
-        'C': 'CWR',
-    }
-    :param input_f:
-    :return:
-    """
-    st = time.time()
-    print('process ... \'%s\'' % input_f,flush=True)
-    # Step 1. read from pcap and return a list of packets
-    try:
-        # pkts_lst = rdpcap(input_f)  # this will read all packets in memory at once.
-        pkts_lst = ''   # not implement
-    except MemoryError as me:
-        print('memory error ', me)
-        return -1
-    except FileNotFoundError as fnfe:
-        print('file not found ', fnfe)
-        return -2
-    except:
-        print('other exceptions')
-        return -10
-
-    # data.stats
-    print('%s info is %s' % (input_f, pkts_lst))
-    pkts_stats = {'non_Ether_pkts': 0, 'non_IPv4_pkts': 0, 'non_TCP_UDP_pkts': 0, 'TCP_pkts': 0,
-                  'UDP_pkts': 0}
-    print('packet info:"srcIP:srcPort-dstIP:dstPort-prtcl" + IP_payload')
-
-    # Step 2. achieve all the session in pcap.
-    cnt = 0
-    sess_dict = {}
-    for pkt in pkts_lst:
-        if pkt.name == "Ethernet":
-            if pkt.payload.name.upper() in ['IP', 'IPV4']:
-                if pkt.payload.payload.name.upper() in ["TCP", "UDP"]:
-                    if cnt == 0:
-                        print('packet[0] info: "%s:%d-%s:%d-%s"+%s' % (
-                            pkt.payload.src, pkt.payload.payload.sport, pkt.payload.dst, pkt.payload.payload.dport,
-                            pkt.payload.payload.name, pkt.payload.payload.payload))
-                    five_tuple = pkt.payload.src + ':' + str(
-                        pkt.payload.payload.sport) + '-' + pkt.payload.dst + ':' + str(
-                        pkt.payload.payload.dport) + '-' + pkt.payload.payload.name.upper()
-                    save_session_to_dict(k=five_tuple, v=pkt, sess_dict=sess_dict)
-                    cnt += 1
-                    # pkts_lst.append(pkt.payload)  # only include "IPv4+IPv4_payload"
-                    if pkt.payload.payload.name.upper() == "TCP":
-                        pkts_stats['TCP_pkts'] += 1
-                    else:
-                        pkts_stats['UDP_pkts'] += 1
-                else:
-                    pkts_stats['non_TCP_UDP_pkts'] += 1
-                    # pkts_stats['IPv4_pkts'] += 1
-            else:
-                pkts_stats['non_IPv4_pkts'] += 1
-        else:
-            pkts_stats['non_Ether_pkts'] += 1
-
-    # Step 3. achieve all full session in sess_dict.
-    full_sess_dict = {}
-    for k, v in sess_dict.items():
-        prtl = k.split('-')[-1]
-        if prtl == "TCP":
-            """
-                only save the first full session in v (maybe there are more than one full session in v)
-            """
-            tcp_sess_list = []
-            full_session_flg = False
-            i = -1
-            TCP_start_flg = False
-            for pkt in v:
-                i += 1
-                if len(v) < 5:  # tcp start (3 packets) + tcp finish (at least 2 packets)
-                    print('%s not full session, it only has %d packets' % (k, len(v)))
-                    break
-                S = str(pkt.payload.payload.fields['flags'])
-                # step 1. discern the begin of TCP session.
-                if 'S' in S:
-                    TCP_start_flg = True
-                    if 'A' not in S:  # the first SYN packet in TCP session.
-                        # if flags[S] == "SYN":
-                        tcp_sess_list.append(pkt)
+                        continue    # cannot ignore
                     else:  # the second SYN + ACK
                         tcp_sess_list.append(pkt)
                     continue
